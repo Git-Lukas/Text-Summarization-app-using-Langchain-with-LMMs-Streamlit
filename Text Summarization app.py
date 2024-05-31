5381f0a969f4dbbad7758c01a680da7176ef7e71
Lucas Todarello <132563585+Git-Lukas@users.noreply.github.com>
2024-05-31 12:45:56 +0200

    Update Text Summarization app.py

--- Text Summarization app.py
+++ Text Summarization app.py
@@ -16,7 +16,7 @@ def summarize_text(txt):
     # This open source model can be downloaded from here
     # Their are multiple models available just replace it in place of model and try it.
     llm = CTransformers(
-        model=r"D:\Lucas\Master_Docs\2-Semestre\Técnicas de Desarrollo Avanzado de Aplicaciones Big Data\AC-2\llama-2-7b-chat.ggmlv3.q2_K.bin",
+        model=r"llama-2-7b-chat.ggmlv3.q2_K.bin",
         model_type="llama",
          max_new_tokens = 512,
         temperature = 0.5   )
